04d71946408deb6c9c5b5632135b80dce5ecb495
nicholas <nickptran@gmail.com>
2022-05-10 22:40:32 -0700

    finished server

--- server.R
+++ server.R
@@ -3,5 +3,11 @@ server <- function(input, output) {
 }
 
 output$plot <- renderPlot({
-    ggplot(data = characters, aes_string())
+    ggplot(
+        data = characters,
+        aes_string(x = "Character", y = input$y_var, fill = "class")
+    ) +
+        geom_bar(stat = "identity", width = 0.8) +
+        labs(x = "Character", y = input$y_var) +
+        coord_flip()
 })
