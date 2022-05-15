e9da4564e849b2502ba0332250ad00561ea50a4b
nicholas <nickptran@gmail.com>
2022-05-15 15:33:40 -0700

    Changed page layout

--- midterm/server.R
+++ midterm/server.R
@@ -1,12 +1,20 @@
 data <- read.csv("data/GlobalMusicData.csv")
 
 server <- function(input, output) {
-# choose columns to display
+    # choose columns to display
     data2 <- data[sample(nrow(data), 1000), ]
     output$mytable1 <- DT::renderDataTable({
-        DT::datatable(data2[, input$showVars, drop = FALSE])
+        DT::datatable(
+            data2[, input$showVars, drop = FALSE],
+            style = "bootstrap",
+            options = list(
+                deferRender = TRUE,
+                scrollY = 500,
+                scrollX = 200,
+                scroller = TRUE
+            )
+        )
     })
 
-# sorted columns are colorized
-
+    # sorted columns are colorized
 }
--- midterm/ui.R
+++ midterm/ui.R
@@ -1,19 +1,25 @@
 library(DT)
+library(shinythemes)
+
 data <- read.csv("data/GlobalMusicData.csv")
 
 # page 1
 introPage <- tabPanel(
     "Introduction",
-    titlePanel("Introduction"),
+    h1("Introduction"),
+    tags$hr(style = "border-color: white;"),
+    h2("Purpose"),
     p("Put stuff here that will introduce the user")
 )
 
-# page 2
+
+# page 2 stuff
 selectValues <- names(data)
 selectValues <- selectValues[
     !selectValues %in% c("track_id", "track_album_id", "playlist_id")
 ]
 
+# page 2 sidebar
 rawDataSideBar <- sidebarPanel(
     conditionalPanel(
         "selectColumns",
@@ -28,25 +34,30 @@ rawDataSideBar <- sidebarPanel(
 )
 
 
+# page 2 main content
 rawDataMain <- mainPanel(
     tabsetPanel(
         id = "dataset",
-        tabPanel("Global Music", DT::dataTableOutput("mytable1"),
-            width = "10"
+        tabPanel(
+            "Global Music",
+            DT::dataTableOutput("mytable1"),
+            width = "100%"
         )
     )
 )
 
-
+# actual page 2
 rawDataPage <- tabPanel(
     "Raw Data",
-    titlePanel("Raw Data"),
-    sidebarLayout(rawDataSideBar, rawDataMain)
+    h1("Raw Data"),
+    tags$hr(style = "border-color: white;"),
+    sidebarLayout(rawDataSideBar, rawDataMain),
 )
 
 
 # load everything to UI
 ui <- navbarPage(
+    theme = shinytheme("darkly"),
     "Nicholas' Midterm",
     introPage,
     rawDataPage
