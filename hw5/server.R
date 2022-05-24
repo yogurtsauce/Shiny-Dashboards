a48c3e9e7f3b86700716c55aa3894992afa4f294
nicholas <nickptran@gmail.com>
2022-05-24 12:14:15 -0700

    added tables to server.R

--- hw5/server.R
+++ hw5/server.R
@@ -4,5 +4,173 @@ library(tidyverse)
 source("code/ImportData.R")
 
 server <- function(input, output, session) {
+    output$customerTable <- renderDT(
+        customer,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
 
+
+    output$productTable <- renderDT(
+        product,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$salesOrderTable <- renderDT(
+        salesOrder,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$salesPersonTable <- renderDT(
+        salesPerson,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$calendarDimTable <- renderDT(
+        calendarDim,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$customerDimTable <- renderDT(
+        customerDim,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$productDimTable <- renderDT(
+        productDim,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$salesFactTable <- renderDT(
+        salesFact,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
+
+
+    output$salesPersonDimTable <- renderDT(
+        salesPersonDim,
+        style = "bootstrap",
+        filter = "top",
+        extensions = "Buttons",
+        options = list(
+            deferRender = TRUE,
+            dom = "Bfrtip",
+            scrollX = "100px",
+            scrollY = "400px",
+            buttons = list("colvis", "print", list(
+                extend = "collection",
+                buttons = c("csv", "excel", "pdf"),
+                text = "Download"
+            ))
+        )
+    )
 }
